cbe07f71d7ce8061425bf81e688f02de0fb339f2
Nikhil Teotia <78010130+Nikhilteotia1101@users.noreply.github.com>
2025-08-04 20:13:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ import numpy as np
 model = pickle.load(open("model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
+# Setup logging
+os.makedirs("logs", exist_ok=True)
+logging.basicConfig(
+    filename="logs/app.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+
 st.title("Loan Eligibility Prediction App")
+
 
 # Input fields
 income = st.number_input("Applicant's Monthly Income", min_value=0)
